ad026c78ab899945820fc758529ee6e3efeb61cf
demidroll-s <60785054+demidroll-s@users.noreply.github.com>
2020-04-13 11:08:55 +0300

    Delete types.h

--- types.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-enum capital_t { car = 1, country_house = 2, flat = 3, stocks = 4 };
-
-enum work_healthy_t { no_life_risk = 1, low_life_risk = 2, average_life_risk = 3, high_life_risk = 4};
-
-enum disease_t { heart = 1, immunity = 2, digestion = 3, skeleton = 4};
